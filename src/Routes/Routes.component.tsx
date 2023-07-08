36c62985b0d05d58829b08d2951bb8e6c685e32d
mohammadsaeed0901 <mohammad.saeed.ka78@gmail.com>
2023-07-09 00:21:22 +0330

    Add New Routes in Routes File

--- src/Routes/Routes.component.tsx
+++ src/Routes/Routes.component.tsx
@@ -5,6 +5,10 @@ import GuardRoute from "./GuardRoute.component";
 const Dashboard = lazy(() => import("../Pages/Dashboard/Dashboard.component"));
 const PlanesList = lazy(() => import("../Pages/Plane/PlanesList.component"));
 const PlaneReserve = lazy(() => import("../Pages/Plane/Reserve/PlaneReserve.component"));
+const TrainsList = lazy(() => import("../Pages/Train/TrainsList.component"));
+const TrainReserve = lazy(() => import("../Pages/Train/Reserve/TrainReserve.component"));
+const HotelsList = lazy(() => import("../Pages/Hotel/HotelsList.component"));
+const HotelReserve = lazy(() => import("../Pages/Hotel/Reserve/HotelReserve.component"));
 const Login = lazy(() => import("../Pages/Login/Login.component"));
 const Resigter = lazy(() => import("../Pages/Register/Register.component"));
 const NotFound = lazy(() => import("../Pages/NotFound/NotFound.component"));
@@ -16,6 +20,10 @@ const Routes: FC = () => {
             <Route path="/" element={<Dashboard />} />
             <Route path="/flights/:city" element={<PlanesList />} />
             <Route path="/flights/:city/:id" element={<PlaneReserve />} />
+            <Route path="/trains/:city" element={<TrainsList />} />
+            <Route path="/trains/:city/:id" element={<TrainReserve />} />
+            <Route path="/hotel/:city" element={<HotelsList />} />
+            <Route path="/hotel/:city/:id" element={<HotelReserve />} />
             <Route path="*" element={<NotFound />} />
           </Route>
           <Route path="/login" element={<Login />} />
